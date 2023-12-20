0a79a427e6b13eb926432fc575cf776903fd7525
TjFish <34532288+TjFish@users.noreply.github.com>
2023-12-21 04:28:18 +0800

    Update Image.tsx

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`/blog${src}`} {...rest} />
-)
+// @ts-ignore
+const Image = ({ ...rest }: ImageProps) => <img {...rest} />
 
 export default Image
